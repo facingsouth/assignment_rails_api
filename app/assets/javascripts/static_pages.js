f35ea8f73610c4a7b681a0be01176c5d07c2362a
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-08 15:00:05 -0400

    tables auto populate

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,4 @@
+var test = 1
 var movieApp = {
   getMovieData: function(){
     $.ajax( {
@@ -24,8 +25,7 @@ var movieApp = {
       success: function( json ) {
 
           // for example, build a post object onto the body
-          $( "<h1/>" ).text( json.title ).appendTo( "body" );
-          $( "<div class=\"content\"/>").html( json.html ).appendTo( "body" );
+          movieApp.addMovies( json );
       },
 
       // Error callback to run if the request fails
@@ -46,11 +46,75 @@ var movieApp = {
     });
   },
 
+  getReviewData: function(){
+    $.ajax( {
+
+      // The URL (path) for the request
+      // The below would submit to http://www.yoursite.com/post
+      url: "reviews",
+
+      // the data to send (will be converted to a query string)
+      // note that this is an object
+      // data: {
+      //     id: 123
+      // },
+
+      // HTTP verb (aka "Type" of request)
+      type: "GET",
+
+      // the type of data we expect back
+      dataType : "json",
+
+      // Success callback to run if the request succeeds.
+      // The response is passed to the function
+      // as a variable, usually called `data` or `json`
+      success: function( json ) {
+
+          // for example, build a post object onto the body
+          movieApp.addReviews( json );
+      },
+
+      // Error callback to run if the request fails
+      // (e.g. server returns an error code like 301)
+      // The raw request and any status codes are
+      // passed to the callback
+      error: function( xhr, status, errorThrown ) {
+          alert( "Sorry, there was a problem!" );
+          console.log( "Error: " + errorThrown );
+          console.log( "Status: " + status );
+          console.dir( xhr );
+      },
+
+      // Complete callback to run regardless of the outcome
+      complete: function( xhr, status ) {
+          alert( "The request is complete!" );
+      }
+    });
+  },
+
+  addReviews: function( json ) {
+    for (var i=0; i<json.length; i++) {
+      var $review = $("<tr>");
+      $review.append("<td>" + json[i].reviewer_name + "</td>");
+      $review.append("<td>" + json[i].title + "</td>");
+      $review.append("<td>" + json[i].text + "</td>");
+      $review.append("<td>" + json[i].review_date + "</td>");
+      $review.append("<td>" + json[i].movie_id + "</td>");
+      $("#review-list").append($review)
+    }
+  },
+
   addMovies: function( json ) {
     for ( var i=0; i<json.length; i++ ) {
       var $movie = $("<tr>");
-      $movie.append("");
-      $("#movie-list").append(movie)
+      $movie.append("<td>" + json[i].name + "</td>");
+      $movie.append("<td>" + json[i].release_date + "</td>");
+      $("#movie-list").append($movie)
     }
   }
 }
+
+$(document).ready(function(){
+  movieApp.getMovieData();
+  movieApp.getReviewData();
+})
